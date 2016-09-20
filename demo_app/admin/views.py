9c04579840547726332c6a2f4de64b4e87bf2c81
Alex <alexperez89@gmail.com>
2016-09-20 15:47:49 +0200

    Author update view and tests

--- demo_app/admin/views.py
+++ demo_app/admin/views.py
@@ -11,6 +11,7 @@ from demo_app.blog.models import Author
 def index():
     return render_template('admin/index.html')
 
+# Authors views
 @admin.route('/authors', methods=['GET'])
 def authors():
     authors = Author.query.all()
@@ -38,7 +39,30 @@ def author_create():
             flash('Author has been successfully created')
             return redirect(url_for('admin.authors'))
     except IntegrityError:
+        db.session.rollback()
         flash('Email must be unique')
-        return render_template('admin/authors/author_create.html', form=form)
+        return render_template('admin/authors/author_create.html', form=form), 404
     return render_template('admin/authors/author_create.html', form=form)
 
+@admin.route('/authors/update/<int:author_id>', methods=['GET', 'POST'])
+def author_update(author_id):
+    author = Author.query.filter_by(id=author_id).first()
+    if author is None:
+        return abort(404)
+    form = AuthorForm(obj=author)
+    try:
+        if form.validate_on_submit():
+            if form.name:
+                author.name = form.name.data
+            if form.description:
+                author.description = form.description.data
+            if form.email:
+                author.email = form.email.data
+            db.session.commit()
+            flash('Author has been successfully updated')
+            return redirect(url_for('admin.authors'))
+    except IntegrityError:
+        db.session.rollback()
+        flash('Email must be unique')
+        return render_template('admin/authors/author_update.html', form=form, author=author), 404
+    return render_template('admin/authors/author_update.html', form=form, author=author)
--- tests/test_views.py
+++ tests/test_views.py
@@ -37,8 +37,42 @@ def test_admin_author_cant_post_create_view_with_duplicate_email(app_client, ses
         'description': 'Hi, I am John Doe',
         'email': 'john@example.com'
     }, follow_redirects=True)
+    assert response.status_code == 404
+
+def test_admin_author_can_post_another_author(app_client, session):
+    response = app_client.post(url_for('admin.author_create'), data={
+        'name': 'Jane Doe',
+        'description': 'Hi, I am Jane Doe',
+        'email': 'jane@example.com'
+    }, follow_redirects=True)
     assert response.status_code == 200
 
 def test_admin_author_can_get_create_view_when_user(app_client, session):
     response = app_client.get(url_for('admin.author_detail', author_id=1))
     assert response.status_code == 200
+
+def test_admin_author_can_get_update_view(app_client, session):
+    response = app_client.get(url_for('admin.author_update', author_id=1))
+    assert response.status_code == 200
+
+def test_admin_author_cant_get_update_view_when_user_does_not_exist(app_client, session):
+    response = app_client.get(url_for('admin.author_update', author_id=11))
+    assert response.status_code == 404
+
+def test_admin_author_can_post_update_view_new_data(app_client, session):
+    response = app_client.post(url_for('admin.author_update', author_id=1), 
+    data={
+        'name': 'Mike Doe',
+        'description': 'Hi, I am John Doe',
+        'email': 'john@example.com'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+
+def test_admin_author_cant_post_update_view_when_email_is_duplicated(app_client, session):
+    response = app_client.post(url_for('admin.author_update', author_id=1), 
+    data={
+        'name': 'John Doe',
+        'description': 'Hi, I am John Doe',
+        'email': 'jane@example.com'
+    }, follow_redirects=True)
+    assert response.status_code == 404
