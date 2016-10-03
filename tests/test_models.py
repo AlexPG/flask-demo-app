1cc309e9be6835d1822838554e7895319135ce1a
Alex <alexperez89@gmail.com>
2016-10-03 18:40:08 +0200

    Test functions moved inside classes

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,119 +5,125 @@ from sqlalchemy.orm.exc import UnmappedInstanceError
 from demo_app.blog.models import Author, Category, Entry
 
 # Author model
-def test_author_can_post(app, session):
-    author = Author(name='John', description='Hi, I am John Doe', \
-                    email='john@example.com')
-    session.add(author)
-    session.commit()
-
-def test_author_cant_post_same_author(app, session):
-    author = Author(name='John', description='Hi, I am John Doe', \
-                    email='john@example.com')
-    session.add(author)
-    with pytest.raises(IntegrityError):
+class TestAuthor:
+    '''Test Author Model'''
+    def test_author_can_post(self, app, session):
+        author = Author(name='John', description='Hi, I am John Doe', \
+                        email='john@example.com')
+        session.add(author)
         session.commit()
 
-def test_author_can_post_another_author(app, session):
-    author = Author(name='Jane', description='Hi, I am Jane Doe', \
-                    email='jane@example.com')
-    session.add(author)
-    session.commit()
+    def test_author_cant_post_same_author(self, app, session):
+        author = Author(name='John', description='Hi, I am John Doe', \
+                        email='john@example.com')
+        session.add(author)
+        with pytest.raises(IntegrityError):
+            session.commit()
 
-def test_author_can_get_existing_author(app, session):
-    john = Author.query.filter_by(name='John').first()
-    assert john.name == 'John'
-
-def test_author_cant_get_non_existing_author(app, session):
-    mike = Author.query.filter_by(name='Mike').first()
-    assert mike == None
-
-def test_author_can_get_existing_author_list(app, session):
-    authors = Author.query.all()
-    assert len(authors) > 0
-
-def test_author_can_update_existing_author(app, session):
-    author = Author.query.filter_by(name='John').first()
-    author.email = 'mike@example.com'
-    session.commit()
-
-def test_author_cant_update_existing_author_with_non_unique_email(app, session):
-    author = Author.query.filter_by(name='John').first()
-    author.email = 'jane@example.com'
-    with pytest.raises(IntegrityError):
+    def test_author_can_post_another_author(self, app, session):
+        author = Author(name='Jane', description='Hi, I am Jane Doe', \
+                        email='jane@example.com')
+        session.add(author)
         session.commit()
 
-def test_author_can_delete_existing_author(app, session):
-    author = Author.query.filter_by(name='John').first()
-    session.delete(author)
-    session.commit()
+    def test_author_can_get_existing_author(self, app, session):
+        john = Author.query.filter_by(name='John').first()
+        assert john.name == 'John'
 
-def test_author_cant_delete_non_existing_author(app, session):
-    author = Author.query.filter_by(name='Mike').first()
-    with pytest.raises(UnmappedInstanceError):
+    def test_author_cant_get_non_existing_author(self, app, session):
+        mike = Author.query.filter_by(name='Mike').first()
+        assert mike == None
+
+    def test_author_can_get_existing_author_list(self, app, session):
+        authors = Author.query.all()
+        assert len(authors) > 0
+
+    def test_author_can_update_existing_author(self, app, session):
+        author = Author.query.filter_by(name='John').first()
+        author.email = 'mike@example.com'
+        session.commit()
+
+    def test_author_cant_update_existing_author_with_non_unique_email(self, app, session):
+        author = Author.query.filter_by(name='John').first()
+        author.email = 'jane@example.com'
+        with pytest.raises(IntegrityError):
+            session.commit()
+
+    def test_author_can_delete_existing_author(self, app, session):
+        author = Author.query.filter_by(name='John').first()
         session.delete(author)
         session.commit()
 
+    def test_author_cant_delete_non_existing_author(self, app, session):
+        author = Author.query.filter_by(name='Mike').first()
+        with pytest.raises(UnmappedInstanceError):
+            session.delete(author)
+            session.commit()
+
 # Category model
-def test_category_can_post(app, session):
-    category = Category(name='IT')
-    session.add(category)
-    session.commit()
-
-def test_category_cant_post_same_category(app, session):
-    category = Category(name='IT')
-    session.add(category)
-    with pytest.raises(IntegrityError):
+class TestCategory:
+    '''Test Category Model'''
+    def test_category_can_post(self, app, session):
+        category = Category(name='IT')
+        session.add(category)
         session.commit()
 
-def test_category_can_post_another_category(app, session):
-    category = Category(name='Sport')
-    session.add(category)
-    session.commit()
+    def test_category_cant_post_same_category(self, app, session):
+        category = Category(name='IT')
+        session.add(category)
+        with pytest.raises(IntegrityError):
+            session.commit()
 
-def test_category_can_update_existing_category(app, session):
-    category = Category.query.filter_by(name='IT').first()
-    category.name = 'Flask'
-    session.commit()
-
-def test_category_cant_update_existing_category_with_non_unique_name(app, session):
-    category = Category.query.filter_by(name='Flask').first()
-    category.name = 'Sport'
-    with pytest.raises(IntegrityError):
+    def test_category_can_post_another_category(self, app, session):
+        category = Category(name='Sport')
+        session.add(category)
         session.commit()
 
-def test_category_can_delete_existing_category(app, session):
-    category = Category.query.filter_by(name='Flask').first()
-    session.delete(category)
-    session.commit()
+    def test_category_can_update_existing_category(self, app, session):
+        category = Category.query.filter_by(name='IT').first()
+        category.name = 'Flask'
+        session.commit()
 
-def test_category_cant_delete_non_existing_category(app, session):
-    category = Category.query.filter_by(name='Flask').first()
-    with pytest.raises(UnmappedInstanceError):
+    def test_category_cant_update_existing_category_with_non_unique_name(self, app, session):
+        category = Category.query.filter_by(name='Flask').first()
+        category.name = 'Sport'
+        with pytest.raises(IntegrityError):
+            session.commit()
+
+    def test_category_can_delete_existing_category(self, app, session):
+        category = Category.query.filter_by(name='Flask').first()
         session.delete(category)
         session.commit()
 
+    def test_category_cant_delete_non_existing_category(self, app, session):
+        category = Category.query.filter_by(name='Flask').first()
+        with pytest.raises(UnmappedInstanceError):
+            session.delete(category)
+            session.commit()
+
 # Entry model
-def test_entry_can_post(app, session):
-    john = Author(name='John', description='Hi, I am John Doe', \
-                  email='john@example.com')
-    session.add(john)
-    session.commit()
+class TestEntry:
+    '''Test Entry Model'''
+    def test_entry_can_post(self, app, session):
+        john = Author(name='John', description='Hi, I am John Doe', \
+                      email='john@example.com')
+        session.add(john)
+        session.commit()
 
-    it = Category(name='IT')
-    flask = Category(name='Flask')
-    python = Category(name='Python')
-    session.add_all([it, flask, python])
-    session.commit()
+        it = Category(name='IT')
+        flask = Category(name='Flask')
+        python = Category(name='Python')
+        session.add_all([it, flask, python])
+        session.commit()
 
-    entry1 = Entry(title='Hello World', body='This is my first entry', \
-                   author=john)
-    entry1.en_ca.append(it)
-    entry1.en_ca.append(python)
-    session.add(entry1)
+        entry1 = Entry(title='Hello World', body='This is my first entry', \
+                       author=john)
+        entry1.en_ca.append(it)
+        entry1.en_ca.append(python)
+        session.add(entry1)
 
-    jane = Author.query.filter_by(name='Jane').first()
-    entry2 = Entry(title='Hello World', body='This is my second entry', \
-                   author=jane)
-    session.add_all([entry1, entry2])
-    session.commit()
+        jane = Author.query.filter_by(name='Jane').first()
+        entry2 = Entry(title='Hello World', body='This is my second entry', \
+                       author=jane)
+        session.add_all([entry1, entry2])
+        session.commit()
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,169 +1,179 @@
 from flask import url_for
 
 # Main views
-def test_app_is_running(app_client):
-    response = app_client.get(url_for('main.hello'))
-    assert response.status_code == 200
+class TestApp: 
+    '''Test App'''
+    def test_app_is_running(self, app_client):
+        response = app_client.get(url_for('main.hello'))
+        assert response.status_code == 200
 
 # Admin views
-def test_admin_index_view(app_client):
-    response = app_client.get(url_for('admin.index'))
-    assert response.status_code == 200
+class TestAdmin:
+    '''Test Admin'''
+    def test_admin_index_view(self, app_client):
+        response = app_client.get(url_for('admin.index'))
+        assert response.status_code == 200
 
 # Admin views authors
-def test_admin_author_can_get_index_view(app_client, session):
-    response = app_client.get(url_for('admin.authors'))
-    assert response.status_code == 200
+class TestAuthor:
+    '''Test Author CRUD'''
+    def test_admin_author_can_get_index_view(self, app_client, session):
+        response = app_client.get(url_for('admin.authors'))
+        assert response.status_code == 200
 
-def test_admin_author_cant_get_detail_view_when_no_author(app_client, session):
-    response = app_client.get(url_for('admin.author_detail', author_id=11))
-    assert response.status_code == 404
+    def test_admin_author_cant_get_detail_view_when_no_author(self, app_client, session):
+        response = app_client.get(url_for('admin.author_detail', author_id=11))
+        assert response.status_code == 404
 
-def test_admin_author_can_get_create_view(app_client, session):
-    response = app_client.get(url_for('admin.author_create'))
-    assert response.status_code == 200
+    def test_admin_author_can_get_create_view(self, app_client, session):
+        response = app_client.get(url_for('admin.author_create'))
+        assert response.status_code == 200
 
-def test_admin_author_can_post_create_view(app_client, session):
-    response = app_client.post(url_for('admin.author_create'), data={
-        'name': 'John Doe',
-        'description': 'Hi, I am John Doe',
-        'email': 'john@example.com'
-    }, follow_redirects=True)
-    assert response.status_code == 200
+    def test_admin_author_can_post_create_view(self, app_client, session):
+        response = app_client.post(url_for('admin.author_create'), data={
+            'name': 'John Doe',
+            'description': 'Hi, I am John Doe',
+            'email': 'john@example.com'
+        }, follow_redirects=True)
+        assert response.status_code == 200
 
-def test_admin_author_cant_post_create_view_with_duplicate_email(app_client, session):
-    response = app_client.post(url_for('admin.author_create'), data={
-        'name': 'John Doe',
-        'description': 'Hi, I am John Doe',
-        'email': 'john@example.com'
-    }, follow_redirects=True)
-    assert response.status_code == 404
+    def test_admin_author_cant_post_create_view_with_duplicate_email(self, app_client, session):
+        response = app_client.post(url_for('admin.author_create'), data={
+            'name': 'John Doe',
+            'description': 'Hi, I am John Doe',
+            'email': 'john@example.com'
+        }, follow_redirects=True)
+        assert response.status_code == 404
 
-def test_admin_author_can_post_another_author(app_client, session):
-    response = app_client.post(url_for('admin.author_create'), data={
-        'name': 'Jane Doe',
-        'description': 'Hi, I am Jane Doe',
-        'email': 'jane@example.com'
-    }, follow_redirects=True)
-    assert response.status_code == 200
+    def test_admin_author_can_post_another_author(self, app_client, session):
+        response = app_client.post(url_for('admin.author_create'), data={
+            'name': 'Jane Doe',
+            'description': 'Hi, I am Jane Doe',
+            'email': 'jane@example.com'
+        }, follow_redirects=True)
+        assert response.status_code == 200
 
-def test_admin_author_can_get_detail_view_when_author(app_client, session):
-    response = app_client.get(url_for('admin.author_detail', author_id=1))
-    assert response.status_code == 200
+    def test_admin_author_can_get_detail_view_when_author(self, app_client, session):
+        response = app_client.get(url_for('admin.author_detail', author_id=1))
+        assert response.status_code == 200
 
-def test_admin_author_can_get_update_view(app_client, session):
-    response = app_client.get(url_for('admin.author_update', author_id=1))
-    assert response.status_code == 200
+    def test_admin_author_can_get_update_view(self, app_client, session):
+        response = app_client.get(url_for('admin.author_update', author_id=1))
+        assert response.status_code == 200
 
-def test_admin_author_cant_get_update_view_when_author_does_not_exist(app_client, session):
-    response = app_client.get(url_for('admin.author_update', author_id=11))
-    assert response.status_code == 404
+    def test_admin_author_cant_get_update_view_when_author_does_not_exist(self, app_client, session):
+        response = app_client.get(url_for('admin.author_update', author_id=11))
+        assert response.status_code == 404
 
-def test_admin_author_can_post_update_view_new_data(app_client, session):
-    response = app_client.post(url_for('admin.author_update', author_id=1), 
-    data={
-        'name': 'Mike Doe',
-        'description': 'Hi, I am John Doe',
-        'email': 'john@example.com'
-    }, follow_redirects=True)
-    assert response.status_code == 200
+    def test_admin_author_can_post_update_view_new_data(self, app_client, session):
+        response = app_client.post(url_for('admin.author_update', author_id=1), 
+        data={
+            'name': 'Mike Doe',
+            'description': 'Hi, I am John Doe',
+            'email': 'john@example.com'
+        }, follow_redirects=True)
+        assert response.status_code == 200
 
-def test_admin_author_cant_post_update_view_when_email_is_duplicated(app_client, session):
-    response = app_client.post(url_for('admin.author_update', author_id=1), 
-    data={
-        'name': 'John Doe',
-        'description': 'Hi, I am John Doe',
-        'email': 'jane@example.com'
-    }, follow_redirects=True)
-    assert response.status_code == 404
+    def test_admin_author_cant_post_update_view_when_email_is_duplicated(self, app_client, session):
+        response = app_client.post(url_for('admin.author_update', author_id=1), 
+        data={
+            'name': 'John Doe',
+            'description': 'Hi, I am John Doe',
+            'email': 'jane@example.com'
+        }, follow_redirects=True)
+        assert response.status_code == 404
 
-def test_admin_author_can_delete_author(app_client, session):
-    response = app_client.get(url_for('admin.author_delete', author_id=3))
-    assert response.status_code == 302
+    def test_admin_author_can_delete_author(self, app_client, session):
+        response = app_client.get(url_for('admin.author_delete', author_id=3))
+        assert response.status_code == 302
 
-def test_admin_author_cant_delete_author_already_deleted(app_client, session):
-    response = app_client.get(url_for('admin.author_delete', author_id=3))
-    assert response.status_code == 404
+    def test_admin_author_cant_delete_author_already_deleted(self, app_client, session):
+        response = app_client.get(url_for('admin.author_delete', author_id=3))
+        assert response.status_code == 404
 
 # Admin views categories
-def test_admin_category_can_get_index_view(app_client, session):
-    response = app_client.get(url_for('admin.categories'))
-    assert response.status_code == 200
+class TestCategory:
+    '''Test Category CRUD'''
+    def test_admin_category_can_get_index_view(self, app_client, session):
+        response = app_client.get(url_for('admin.categories'))
+        assert response.status_code == 200
 
-def test_admin_category_can_get_create_view(app_client, session):
-    response = app_client.get(url_for('admin.category_create'))
-    assert response.status_code == 200
+    def test_admin_category_can_get_create_view(self, app_client, session):
+        response = app_client.get(url_for('admin.category_create'))
+        assert response.status_code == 200
 
-def test_admin_category_can_post_create_view(app_client, session):
-    response = app_client.post(url_for('admin.category_create'), data={
-        'name': 'IT'
-    }, follow_redirects=True)
-    assert response.status_code == 200
+    def test_admin_category_can_post_create_view(self, app_client, session):
+        response = app_client.post(url_for('admin.category_create'), data={
+            'name': 'IT'
+        }, follow_redirects=True)
+        assert response.status_code == 200
 
-def test_admin_category_cant_post_create_view_with_duplicate_name(app_client, session):
-    response = app_client.post(url_for('admin.category_create'), data={
-        'name': 'IT'
-    }, follow_redirects=True)
-    assert response.status_code == 404
+    def test_admin_category_cant_post_create_view_with_duplicate_name(self, app_client, session):
+        response = app_client.post(url_for('admin.category_create'), data={
+            'name': 'IT'
+        }, follow_redirects=True)
+        assert response.status_code == 404
 
-def test_admin_category_can_post_another_category(app_client, session):
-    response = app_client.post(url_for('admin.category_create'), data={
-        'name': 'Sport'
-    }, follow_redirects=True)
-    assert response.status_code == 200
+    def test_admin_category_can_post_another_category(self, app_client, session):
+        response = app_client.post(url_for('admin.category_create'), data={
+            'name': 'Sport'
+        }, follow_redirects=True)
+        assert response.status_code == 200
 
-def test_admin_category_can_get_update_view(app_client, session):
-    response = app_client.get(url_for('admin.category_update', category_id=1))
-    assert response.status_code == 200
+    def test_admin_category_can_get_update_view(self, app_client, session):
+        response = app_client.get(url_for('admin.category_update', category_id=1))
+        assert response.status_code == 200
 
-def test_admin_category_cant_get_update_view_when_category_does_not_exist(app_client, session):
-    response = app_client.get(url_for('admin.category_update', category_id=11))
-    assert response.status_code == 404
+    def test_admin_category_cant_get_update_view_when_category_does_not_exist(self, app_client, session):
+        response = app_client.get(url_for('admin.category_update', category_id=11))
+        assert response.status_code == 404
 
-def test_admin_category_can_post_update_view_new_data(app_client, session):
-    response = app_client.post(url_for('admin.category_update', category_id=1),
-    data={
-        'name': 'Flask'
-    }, follow_redirects=True)
-    assert response.status_code == 200
+    def test_admin_category_can_post_update_view_new_data(self, app_client, session):
+        response = app_client.post(url_for('admin.category_update', category_id=1),
+        data={
+            'name': 'Flask'
+        }, follow_redirects=True)
+        assert response.status_code == 200
 
-def test_admin_category_cant_post_update_view_when_name_is_duplicated(app_client, session):
-    response = app_client.post(url_for('admin.category_update', category_id=1),
-    data={
-        'name': 'Sport'
-    }, follow_redirects=True)
-    assert response.status_code == 404
+    def test_admin_category_cant_post_update_view_when_name_is_duplicated(self, app_client, session):
+        response = app_client.post(url_for('admin.category_update', category_id=1),
+        data={
+            'name': 'Sport'
+        }, follow_redirects=True)
+        assert response.status_code == 404
 
-def test_admin_category_can_delete_category(app_client, session):
-    response = app_client.get(url_for('admin.category_delete', category_id=3))
-    assert response.status_code == 302
+    def test_admin_category_can_delete_category(self, app_client, session):
+        response = app_client.get(url_for('admin.category_delete', category_id=3))
+        assert response.status_code == 302
 
-def test_admin_category_cant_delete_category_already_deleted(app_client, session):
-    response = app_client.get(url_for('admin.category_delete', category_id=3))
-    assert response.status_code == 404
+    def test_admin_category_cant_delete_category_already_deleted(self, app_client, session):
+        response = app_client.get(url_for('admin.category_delete', category_id=3))
+        assert response.status_code == 404
 
 # Admin views entries
-def test_admin_entry_can_get_index_view(app_client, session):
-    response = app_client.get(url_for('admin.entries'))
-    assert response.status_code == 200
+class TestEntry:
+    '''Test Entry CRUD'''
+    def test_admin_entry_can_get_index_view(self, app_client, session):
+        response = app_client.get(url_for('admin.entries'))
+        assert response.status_code == 200
 
-def test_admin_entry_can_get_create_view(app_client, session):
-    response = app_client.get(url_for('admin.entry_create'))
-    assert response.status_code == 200
+    def test_admin_entry_can_get_create_view(self, app_client, session):
+        response = app_client.get(url_for('admin.entry_create'))
+        assert response.status_code == 200
 
-def test_admin_entry_can_post_create_view(app_client, session):
-    response = app_client.post(url_for('admin.entry_create'), data={
-        'title': 'Hello World',
-        'body': 'This is my first entry',
-        'author': 1,
-        'category': [1, ]
-    }, follow_redirects=True)
-    assert response.status_code == 200
+    def test_admin_entry_can_post_create_view(self, app_client, session):
+        response = app_client.post(url_for('admin.entry_create'), data={
+            'title': 'Hello World',
+            'body': 'This is my first entry',
+            'author': 1,
+            'category': [1, ]
+        }, follow_redirects=True)
+        assert response.status_code == 200
 
-def test_admin_entry_cant_post_create_view_non_existing_author(app_client, session):
-    response = app_client.post(url_for('admin.entry_create'), data={
-        'title': 'Hello World',
-        'body': 'This is my second entry',
-        'author': 2
-    }, follow_redirects=True)
-    assert response.status_code == 200
+    def test_admin_entry_cant_post_create_view_non_existing_author(self, app_client, session):
+        response = app_client.post(url_for('admin.entry_create'), data={
+            'title': 'Hello World',
+            'body': 'This is my second entry',
+            'author': 2
+        }, follow_redirects=True)
+        assert response.status_code == 200
